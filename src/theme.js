5b4c25374abf2ae9f3569ea781a2e6e5f25e200d
Zyllio <96659995+zyllio@users.noreply.github.com>
2022-08-24 23:09:42 +0200

    -

--- src/theme.js
+++ src/theme.js
@@ -2,19 +2,23 @@
 
 console.log('Plugin Theme started')
 
-const theme = {
-  name: 'Custom Theme',
-  theme: {
-    primaryColor: '#4f7d96',
-    primaryTextColor: '#ffffff',
-    secondaryColor: '#fe844b',
-    secondaryTextColor: '#ffffff',
-    tertiaryColor: '#0000000a',
-    tertiaryTextColor: '#000000',
-    backgroundColor: '#f0f6f9',
-    textColor: '#474f5b',
-    rtl: false
-  }
-}
+(function () {
 
-zySdk.services.registry.registerTheme(theme)
+  const theme = {
+    name: 'Custom Theme',
+    theme: {
+      primaryColor: '#4f7d96',
+      primaryTextColor: '#ffffff',
+      secondaryColor: '#fe844b',
+      secondaryTextColor: '#ffffff',
+      tertiaryColor: '#0000000a',
+      tertiaryTextColor: '#000000',
+      backgroundColor: '#f0f6f9',
+      textColor: '#474f5b',
+      rtl: false
+    }
+  }
+
+  zySdk.services.registry.registerTheme(theme)
+
+})();
